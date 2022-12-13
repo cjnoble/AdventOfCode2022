20473aa46949c95a1313a3326ec08fbc3e844ddb
cjnoble <christopher.noble@arup.com>
2022-12-13 11:57:01 +0000

    Day 13 finished

--- 13_test.py
+++ 13_test.py
@@ -8,6 +8,17 @@ class TestMethods(unittest.TestCase):
         total = day_13.part_1(day_13.parse_data(test_data))
         self.assertEqual(total, 13)
 
+    def test_2(self):
+        test_data = day_13.read_text_file("13_test_2.txt")
+        total = day_13.part_1(day_13.parse_data(test_data))
+        self.assertEqual(total, 0)
+
+    
+    def test_3(self):
+        test_data = day_13.read_text_file("13_test.txt")
+        total = day_13.part_2(day_13.parse_data(test_data))
+        self.assertEqual(total, 140)
+
 if __name__ == '__main__':
 
     unittest.main()
--- day_13.py
+++ day_13.py
@@ -1,5 +1,6 @@
 import json
 from enum import Enum
+from functools import cmp_to_key
 
 def read_text_file (file_path):
 
@@ -34,22 +35,51 @@ class Match(Enum):
 
 def check_pair(pair1, pair2):
 
-    for left, right in zip(pair1, pair2):
-        if isinstance(left, list) or isinstance(right, list):
-            if not isinstance(left, list):
-                left = [left]
-            elif not isinstance(right, list):
-                right = [right]
+    if isinstance(pair1, list) or isinstance(pair2, list):
+        if not isinstance(pair1, list):
+            pair1 = [pair1]
+        elif not isinstance(pair2, list):
+            pair2 = [pair2]
 
+        if len(pair1) == 0:
+            return Match.Match
+        elif len(pair2) == 0:
+            return Match.NoMatch
+
+
+        for left, right in zip(pair1, pair2):
             c = check_pair(left, right)
-            if c.RunOut and len(left) < len(right):
-                return Match.Match
+            if c == Match.Match or c == Match.NoMatch:
+                return c
+            elif c == Match.RunOut:
+                if len(pair1) < len(pair2):
+                    return Match.Match
+                elif len(pair1) > len(pair2):
+                    return Match.NoMatch
 
-        else:
-            if left < right:
-                return Match.Match
+    # for left, right in zip(pair1, pair2):
+    #     if isinstance(left, list) or isinstance(right, list):
+    #         if not isinstance(left, list):
+    #             left = [left]
+    #         elif not isinstance(right, list):
+    #             right = [right]
 
-    return Match.NoMatch
+    #         c = check_pair(left, right)
+    #         if c == Match.Match or c == Match.NoMatch:
+    #             return c
+    #         elif c == Match.RunOut:
+    #             if len(left) < len(right):
+    #                 return Match.Match
+    #             elif len(left) > len(right):
+    #                 return Match.NoMatch
+
+    else:
+        if pair1 < pair2:
+            return Match.Match
+        elif pair1 > pair2:
+            return Match.NoMatch
+
+    return Match.RunOut
 
 def part_1(data):
 
@@ -61,10 +91,30 @@ def part_1(data):
             correct_pairs += index + 1
 
     return correct_pairs
-        
+
+
+def check_pair_cmp(p1, p2):
+
+    results = check_pair(p1, p2)
+    if results == Match.Match:
+        return -1
+    else:
+        return 1 
+
+def part_2(data:list):
+
+    m1 = [[2]]
+    m2 = [[6]]
+
+    data.extend([m1, m2])
+
+    data.sort(key=cmp_to_key(check_pair_cmp))
+
+    return (data.index(m1) + 1) * (data.index(m2) + 1)
 
 if __name__ == "__main__":
 
     data = read_text_file("13.txt")
     data = parse_data(data)
     print(part_1(data))
+    print(part_2(data))
