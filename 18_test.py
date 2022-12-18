1c6feb65eb0370967d95bf95cf02589d3f45245f
Chris Noble <NobleChrisJ@gmail.com>
2022-12-18 12:20:21 +0000

    Day 18 part 1

---
+++ 18_test.py
@@ -0,0 +1,21 @@
+import unittest
+import day_18
+
+class TestMethods(unittest.TestCase):
+
+    def test_1(self):
+        test_data = [[1,1,1], [2,1,1]]
+        #test_data = day_18.parse_data(test_data)
+        total = day_18.part_1(test_data)
+        self.assertEqual(total, 10)
+
+    def test_2(self):
+        test_data = day_18.read_text_file("18_test.txt")
+        #test_data = day_18.parse_data(test_data)
+        total = day_18.part_1(test_data)
+        self.assertEqual(total, 64)
+
+
+if __name__ == '__main__':
+
+    unittest.main()
